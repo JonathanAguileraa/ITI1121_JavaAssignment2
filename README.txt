425820dd3c5acfa32d6476954514e850f953c138
Jonathan Aguilera <128196283+JonathanAguileraa@users.noreply.github.com>
2023-03-17 20:59:53 -0400

    Add files via upload
    Assignment 2

---
+++ README.txt
@@ -0,0 +1,6 @@
+Student name: Jonathan Aguilera
+Student number: 300190572
+Course code: ITI1121
+Lab section: C-4
+
+This archive contains the 13 files for assignment 2 
---
+++ Util.java
@@ -0,0 +1,38 @@
+public class Util {
+
+	public static CarType getCarTypeByLabel(String label) {
+
+		if (label.equals("E"))
+			return CarType.ELECTRIC;
+
+		else if (label.equals("S"))
+			return CarType.SMALL;
+
+		else if (label.equals("R"))
+			return CarType.REGULAR;
+
+		else if (label.equals("L"))
+			return CarType.LARGE;
+
+		else
+			return CarType.NA;
+	}
+
+	public static String getLabelByCarType(CarType type) {
+
+		if (type == CarType.ELECTRIC)
+			return "E";
+
+		else if (type == CarType.SMALL)
+			return "S";
+
+		else if (type == CarType.REGULAR)
+			return "R";
+
+		else if (type == CarType.LARGE)
+			return "L";
+
+		else
+			return "N";
+	}
+}
